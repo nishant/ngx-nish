d56768f0ee124283b411f8d5c85fc6e2f1c9f64a
Nishant Arora <nish54@gmail.com>
2021-09-18 00:29:40 -0500

    created empty modal component

---
+++ projects/ngx-nish/src/lib/components/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-nish-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
