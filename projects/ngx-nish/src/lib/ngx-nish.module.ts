cb17468797f1ff0d2df71e2bdbde74c9dcb56bc3
Nishant Arora <nish54@gmail.com>
2021-09-18 01:08:08 -0500

    dependency updates

--- projects/ngx-nish/src/lib/ngx-nish.module.ts
+++ projects/ngx-nish/src/lib/ngx-nish.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { NgxNishComponent } from './ngx-nish.component';
 import { VideoPlayerComponent } from './components/video-player/video-player.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {CommonModule} from "@angular/common";
+import { CommonModule } from "@angular/common";
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageSectionDividerComponent } from './components/page-section-divider/page-section-divider.component';
 import { PageSectionComponent } from './components/page-section/page-section.component';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { ButtonComponent } from './components/button/button.component';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { RadioButtonComponent } from './components/radio-button/radio-button.component';
 import { RibbonComponent } from './components/ribbon/ribbon.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 
 @NgModule({
@@ -28,11 +30,13 @@ import { RibbonComponent } from './components/ribbon/ribbon.component';
     CheckboxComponent,
     ModalComponent,
     RadioButtonComponent,
-    RibbonComponent
+    RibbonComponent,
+    BannerComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatToolbarModule
   ],
   exports: [
     NgxNishComponent,
